d31223c1371ccd9877afe0568ab181d4aa43eea9
himani2853 <himani.sharma@students.iiit.ac.in>
2025-12-01 21:13:38 +0530

    Fifth commit

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,18 @@
+// import axios from 'axios';
+
+// const api = axios.create({
+//     baseURL: 'http://localhost:5000/api',
+// });
+
+// export default api;
+// client/src/api/axios.js
 import axios from 'axios';
 
-const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
-});
+// automatically detects if you are local or on the web
+const BASE_URL = import.meta.env.MODE === 'development'
+    ? 'http://localhost:5000/api'
+    : 'https://YOUR-APP-NAME.onrender.com/api'; // Paste your Render URL here
 
-export default api;
+export default axios.create({
+    baseURL: BASE_URL,
+});
